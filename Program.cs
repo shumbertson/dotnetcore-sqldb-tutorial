aa186a24f6d5cda4c14b34d4169e5d815f7cf365
Steven Humbertson <shumbert@comcast.net>
2024-02-19 13:30:36 -0500

    convert program.cs ot use main method; add missing nuget dependencies....thanks microsoft for the wonderful tutorial

--- Program.cs
+++ Program.cs
@@ -1,45 +1,52 @@
 ﻿using DotNetCoreSqlDb.Models;
 using Microsoft.EntityFrameworkCore;
-using DotNetCoreSqlDb.Data;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Logging.AddAzureWebAppDiagnostics();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-
-builder.Services.AddStackExchangeRedisCache(options => {
-    options.Configuration = builder.Configuration("AZURE_REDIS_CONNECTIONSTRING");
-    options.InstanceName = "SampleInstance";
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseDeveloperExceptionPage();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddAzureWebAppDiagnostics();
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<MyDatabaseContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+            options.InstanceName = "SampleInstance";
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        // app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Todos}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-else {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-// app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Todos}/{action=Index}/{id?}");
-
-app.Run();
